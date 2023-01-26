dcb08b2619ebc383c2deaf4fdc5fc0a51de41cba
Arnav Sharma <sharmaarnav14241424@gmail.com>
2023-01-26 14:19:41 +0530

    dataTypes

---
+++ dataTypes.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var data int
+	data = 10
+
+	data1 := 100
+
+	data = 1000
+	Data := 10000
+	fmt.Println(data)
+	fmt.Println(data1)
+	fmt.Println(Data)
+	//data Types in GoLang
+	//1.Primitive
+	//int,float64,string,bool,vint,byte
+	//2.Non-Primitive
+	//struct,map,chan,interface,slice,array
+	name := "Arnav"
+	fmt.Println(name)
+}
