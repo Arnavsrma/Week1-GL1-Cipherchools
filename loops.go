69018c286620cd019764ff4ebf304b7787dfc60e
Arnav Sharma <sharmaarnav14241424@gmail.com>
2023-01-26 15:40:28 +0530

    loops

--- loops.go
+++ loops.go
@@ -3,9 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 3; i++ {
-		for i := 0; i < 3; i++ {
-			fmt.Println(i)
+	nums := []int{1, 3, 2, 4, 0}
+	for _, value := range nums {
+		if value == 3 {
+			continue //break
 		}
+		fmt.Println(value)
 	}
+	//list:=range(2)
 }
